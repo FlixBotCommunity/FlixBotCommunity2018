685df53ae982db728ed2966bdaa431b51bf5af4e
FlixBotCommunity <42574388+FlixBotCommunity@users.noreply.github.com>
2018-08-21 17:37:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client();ف
 const prefix = '&';
 const axios = require('axios');
 const fs = require('fs');
