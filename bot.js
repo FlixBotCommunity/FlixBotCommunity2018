759adb0da73b2ef0fddea949b8ec2879c929a3c9
FlixBotCommunity <42574388+FlixBotCommunity@users.noreply.github.com>
2018-08-25 10:32:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,9 +98,9 @@ client.on("message", async message => {
 	if(command == prefix + 'role-react') {
 		if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('`ADMINISTRATOR` **انت لا تمتلك صلاحية**');
 		if(!args) return message.channel.send(`**➥ Useage:** ${prefix}role-react <role-name>`);
-		var role = message.guild.roles.find('name', args) || message.guild.roles.get(args);
-		if(!role) return message.channel.send(`no role with name **${args}** found, make sure you entered correct name`);
-		message.channel.send(`now go and add reaction in the message you want for role ${role.name}`);
+		var role = message.guild.roles.find('name', args) || message.guild.roles.find('id', args);
+		if(!role) return message.channel.send(`No role with name **${args}** found, make sure you entered correct name`);
+		message.channel.send(`Now go and add reaction in the message you want for role ${role.name}`);
 		definedReactionRole = role;
 		stopReacord = false;
 	}
@@ -111,6 +111,7 @@ client.on('raw', raw => {
 	if(channel.messages.has(raw.d.message_id)) return;
 	channel.fetchMessage(raw.d.message_id).then(message => {
 		var reaction = message.reactions.get( (raw.d.emoji.id ? `${raw.d.emoji.name}:${raw.d.emoji.id}` : raw.d.emoji.name) );
+		message.channel.send(`:white_check_mark: Successfully add reaction :${raw.d.emoji.name}: To message id **${raw.d.message_id}** In Channel <#${channel}>`);
 		if(raw.t === 'MESSAGE_REACTION_ADD') return client.emit('messageReactionAdd', reaction, client.users.get(raw.d.user_id));
 		if(raw.t === 'MESSAGE_REACTION_REMOVE') return client.emit('messageReactionRemove', reaction, client.users.get(raw.d.user_id));
 	});
